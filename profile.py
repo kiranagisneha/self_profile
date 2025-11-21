56c603d3c976df2873b12bfa056d48beeb7c08f9
kiranagisneha <kiranagisneha@gmail.com>
2025-11-21 10:01:55 +0530

    save

---
+++ profile.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv)>1:
+    hobbies=sys.argv[1]
+    strenghtness=sys.argv[2]
+    weakness=sys.argv[3]
+else:
+    hobbies="implementing skills"
+    strenghtness="coding skills"
+    weakness="weak in debug"
+    self_profile=("hobbies","strenghtness","weakness")
+    print("hobbiees",hobby)
+    print("strenghtness",strength)
+    print("weakness",weak)
+    print("self_profile",self_profile)
+
+
